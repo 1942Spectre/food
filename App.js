e8c3e057d9642879267de76bed9721d1ff58e401
Afsin1942 <afsin1942@icloud.com>
2022-01-29 02:42:44 +0300

    commit

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {CreateAppContainer} from 'react-navigation';
-import {CreateStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 import SearchScreen from './src/screens/SearchScreen';
 
-const navigator = CreateStackNavigator({
+const navigator = createStackNavigator({
   Search: SearchScreen
 },{
-  initialRouteName:'Search',
+  initialRouteName:"Search",
   defaultNavigationOptions:{
-    title:'Business Search'
+    title:'BusinessSearch'
   }
 })
 
-export default CreateStackNavigator
+export default createAppContainer(navigator)
